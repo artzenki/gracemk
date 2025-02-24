e8d9f2adaf74f2fef8aee4a5c14b9276a39fee2c
Albin <albiameti7@gmail.com>
2025-02-24 08:31:25 +0100

    small fix

--- src/app/[category]/[subcategory]/client-page.js
+++ src/app/[category]/[subcategory]/client-page.js
@@ -3,44 +3,46 @@ import CategoryLayout from "@/app/category-layout";
 export default function SubcategoryPage({ data }) {
   return (
     <CategoryLayout title={data.name} image={data.image}>
-      <div className="max-w-[500px] mx-auto py-16">
-        <ul className="grid grid-cols-1 gap-4">
-          {data.products?.map((product, index) => (
-            <li key={index} className="bg-black p-2 space-y-4">
-              <div className="flex items-center justify-between">
-                <h5 className="font-semibold text-lg tracking-widest text-orange-200">
-                  {product.name}
-                </h5>
-                <p className="text-orange-200 text-sm">{product.reagent}</p>
-              </div>
-              <p className="text-sm uppercase tracking-widest">
-                {product.desc}
-              </p>
-              <div className="flex items-center justify-between">
-                <p className="text-xs tracking-widest">1X</p>
-                <div className="border-b border-white w-full mx-4 h-4"></div>
-                <p className="whitespace-nowrap text-orange-200 tracking-widest">
-                  {product.price}
+      <div className="container py-4 md:py-16">
+        <div className="max-w-[500px] mx-auto">
+          <ul className="grid grid-cols-1 gap-4">
+            {data.products?.map((product, index) => (
+              <li key={index} className="bg-black p-2 space-y-4">
+                <div className="flex items-center justify-between">
+                  <h5 className="font-semibold text-lg tracking-widest text-orange-200">
+                    {product.name}
+                  </h5>
+                  <p className="text-orange-200 text-sm">{product.reagent}</p>
+                </div>
+                <p className="text-sm uppercase tracking-widest">
+                  {product.desc}
                 </p>
-              </div>
-            </li>
-          ))}
-          {data.reagents && (
-            <div className="bg-black p-2 space-y-2">
-              {Object.keys(data.reagents).map((key) => (
-                <div key={key} className="flex items-center space-x-1">
-                  <p className="text-xs text-orange-200 tracking-widest">
-                    *{key}
-                  </p>
-                  <div>-</div>
-                  <p className="text-xs tracking-widest uppercase">
-                    {data.reagents[key]}
+                <div className="flex items-center justify-between">
+                  <p className="text-xs tracking-widest">1X</p>
+                  <div className="border-b border-white w-full mx-4 h-4"></div>
+                  <p className="whitespace-nowrap text-orange-200 tracking-widest">
+                    {product.price}
                   </p>
                 </div>
-              ))}
-            </div>
-          )}
-        </ul>
+              </li>
+            ))}
+            {data.reagents && (
+              <div className="bg-black p-2 space-y-2">
+                {Object.keys(data.reagents).map((key) => (
+                  <div key={key} className="flex items-center space-x-1">
+                    <p className="text-xs text-orange-200 tracking-widest">
+                      *{key}
+                    </p>
+                    <div>-</div>
+                    <p className="text-xs tracking-widest uppercase">
+                      {data.reagents[key]}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            )}
+          </ul>
+        </div>
       </div>
     </CategoryLayout>
   );
